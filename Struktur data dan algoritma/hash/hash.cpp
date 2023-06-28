5cf5247075c5fce28cefa88ff0522953bc00bfd4
fieter <fieterbrain955@gmail.com>
2023-06-28 20:26:31 +0800

    first commit

--- Struktur data dan algoritma/hash/hash.cpp
+++ Struktur data dan algoritma/hash/hash.cpp
@@ -1,38 +1,65 @@
 #include <iostream>
-#include <string>
 #include <vector>
+using namespace std;
 
-// Fungsi hashing sederhana menggunakan metode penjumlahan karakter ASCII
-int hashFunction(const std::string& key, int tableSize) {
-    int hashValue = 0;
-    for (int i = 0; i < key.length(); i++) {
-        hashValue += key[i];
-    }
-    return hashValue % tableSize;
+void hashfunctionLinear(const vector<int>& keys, int tablesize){
+	vector<vector<int> > hashtable(tablesize);
+	int prima = tablesize+1;
+	
+	for(int i =0; i<keys.size();++i){
+		int key = keys[i];
+		int index = key % prima;
+		
+		while(!hashtable[index].empty()){
+			index = (index+1) % prima;
+		}
+		hashtable[index].push_back(key);
+	}
+	
+	for(int i =0;i<hashtable.size();++i){
+		for(int j=0;j<hashtable[i].size();++j){
+			cout << hashtable[i][j];
+		}
+		cout << endl;
+	}
 }
 
-int main() {
-    int tableSize = 10; // Ukuran tabel hash
-    std::vector<std::string> keys = {"apple", "banana", "cat", "dog", "elephant"};
 
-    // Membuat tabel hash dengan menggunakan vector
-    std::vector<std::vector<std::string>> hashTable(tableSize);
 
-    // Menambahkan kata-kata ke dalam tabel hash
-    for (int i = 0; i < keys.size(); i++) {
-        const std::string& key = keys[i];
-        int index = hashFunction(key, tableSize);
-        hashTable[index].push_back(key);
-    }
+void hashfunctionQuadratic(const vector<int>& keys, int tablesize){
+	vector<vector<int> > hashtable(tablesize);
+	int prima = tablesize+1;
+	
+	for(int i =0; i <keys.size();++i){
+		int key = keys[i];
+		int index = key % prima;
+		
+		while(!hashtable[index].empty()){
+			index = (index+index*index) % prima;
+		}
+		hashtable[index].push_back(key);
+	}
+	
+	for(int i =0;i<hashtable.size();++i){
+		for(int j =0; j<hashtable[i].size();++j){
+			cout << hashtable[i][j];
+		}
+		cout << endl;
+	}
 
-    // Menampilkan isi tabel hash
-    for (int i = 0; i < tableSize; i++) {
-        std::cout << "Index " << i << ": ";
-        for (const std::string& key : hashTable[i]) {
-            std::cout << key << " ";
-        }
-        std::cout << std::endl;
-    }
+	
+}
 
-    return 0;
+int main(){
+	int tablesize = 10;
+	cout << "Linear Probing:"<<endl;
+	
+	vector<int> keys = {32, 53, 22, 92, 17, 34, 24, 37,  56 };
+	hashfunctionLinear(keys, tablesize);
+	
+	cout << "\nQuadratic Probing:"<<endl;
+	vector<int> keyss = {0, 15, 16, 20, 30, 25, 26, 36 };
+	hashfunctionQuadratic(keyss, tablesize);
+	
+	return 0;
 }
